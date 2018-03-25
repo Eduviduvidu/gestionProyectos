113dd5e6175dd1f108a4ff1cbd34754ab1439cc5
Unknown <94villadaedu@gmail.com>
2018-03-25 18:33:20 +0200

    CRUD Registro Login
    Creacion del crud para el registro y el login de usuarios en el aplicativo.

---
+++ src/gestionProyectos/mainBundle/Controller/LogoutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace gestionProyectos\mainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller {
+
+    public function indexAction() {
+        return $this->render('gestionProyectosmainBundle:Default:index.html.twig', array("title" => "Inicio"));
+    }
+
+}
